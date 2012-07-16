62bf4c71dcf52d95730d8d0a4785845cce86d088
Sina Samangooei <sinjax@gmail.com>
2012-07-16 18:31:37 +0100

    fixing

--- bin/launchLocal.sh
+++ bin/launchLocal.sh
@@ -2,10 +2,10 @@ DATA="/data/ssh-imagenet-timingTest/data"
 IMAGES="$DATA/image-net-1000000.seq"
 FEATURES="$DATA/features.image-net-1000000.seq"
 JAVA_JAR="bin/LocalFeaturesTool.jar"
-JAVA="java -jar $JAVA_JAR -m SIFT -rm"
+JAVA="java -Xmx2G -jar $JAVA_JAR -m SIFT"
 
 echo "MACHINE RUNNING FROM $1 to $2"
 for (( i = $1; i < $2; i++ )); do
-	echo $JAVA -i $IMAGES/$i -i $FEATURES/$i > $DATA/`hostname`.lastcmd
+	echo $JAVA -i $IMAGES/$i -o $FEATURES/$i > $DATA/`hostname`.lastcmd
 	# touch $FEATURES/$i
 done
