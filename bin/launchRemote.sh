9b3f918545d93d8d1821f32d8f28ecd70f23aa4c
Sina Samangooei <sinjax@gmail.com>
2012-07-16 18:10:02 +0100

    initial version

--- bin/launchRemote.sh
+++ bin/launchRemote.sh
@@ -14,7 +14,8 @@ imagespermachine=$(($nimages/$nmachines))
 # for (( i = 0; i < $nimages-1; i+=$imagespermachine )); do
 for (( i = 0; i < $nmachines; i++ )); do
 	# ssh remoteshot '/tmp/script.sh </dev/null >nohup.out 2>&1 &'
-	echo "ssh ${MACHINES[$i]} \"nohup $LOCALRUN $(($i * $imagespermachine)) $((($i+1)*$imagespermachine)) </dev/null >nohup.out 2>&1 &\""
+	# echo "ssh ${MACHINES[$i]} \"nohup $LOCALRUN $(($i * $imagespermachine)) $((($i+1)*$imagespermachine)) </dev/null >nohup.out 2>&1 &\""
+	ssh ${MACHINES[$i]} "nohup $LOCALRUN $(($i * $imagespermachine)) $((($i+1)*$imagespermachine)) </dev/null >nohup.out 2>&1 &"
 done
 	
 # done
