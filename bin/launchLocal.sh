6c1fccb82a44f371f082d80af63710744fdeac21
Sina Samangooei <sinjax@gmail.com>
2012-07-16 17:53:48 +0100

    initial version

---
+++ bin/launchLocal.sh
@@ -0,0 +1,10 @@
+DATA="data"
+IMAGES="$DATA/image-net-1000000.seq"
+FEATURES="$DATA/features.image-net-1000000.seq"
+JAVA_JAR="bin/LocalFeaturesTool.jar"
+JAVA="java -jar $JAVA_JAR -m SIFT -rm"
+
+echo "MACHINE RUNNING FROM $1 to $2"
+for (( i = $1; i < $2; i++ )); do
+	echo $JAVA -i $IMAGES/$i -i $FEATURES/$i
+done
---
+++ bin/launchRemote.sh
@@ -0,0 +1,14 @@
+LOCALRUN="`pwd`/bin/launchLocal.sh"
+MACHINES=("memling" "dali" "banksy")
+nmachines=${#MACHINES[@]}
+
+nimages=1000000
+imagespermachine=$(($nimages/$nmachines))
+
+# for (( i = 0; i < $nimages-1; i+=$imagespermachine )); do
+for (( i = 0; i < $nmachines; i++ )); do
+	# ssh remoteshot '/tmp/script.sh </dev/null >nohup.out 2>&1 &'
+	echo ssh ${MACHINES[$i]} nohup $LOCALRUN $(($i * $imagespermachine)) $((($i+1)*$imagespermachine))
+done
+	
+# done
