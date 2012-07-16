bbd896457d56a92f7244bea5d6a6ed727dda330b
Sina Samangooei <sinjax@gmail.com>
2012-07-16 18:33:06 +0100

    fixing

--- bin/launchLocal.sh
+++ bin/launchLocal.sh
@@ -7,5 +7,6 @@ JAVA="java -Xmx2G -jar $JAVA_JAR -m SIFT"
 echo "MACHINE RUNNING FROM $1 to $2"
 for (( i = $1; i < $2; i++ )); do
 	echo $JAVA -i $IMAGES/$i -o $FEATURES/$i > $DATA/`hostname`.lastcmd
+	$JAVA -i $IMAGES/$i -o $FEATURES/$i
 	# touch $FEATURES/$i
 done
